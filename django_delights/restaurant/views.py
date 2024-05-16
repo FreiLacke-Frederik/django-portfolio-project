24d60e2c22461a6c62472408ff48ece9f20e7088
Frederik Faller <debug.acc.frei@gmail.com>
2024-05-16 08:20:53 +0200

    Added requirements.txt

--- django_delights/restaurant/views.py
+++ django_delights/restaurant/views.py
@@ -262,8 +262,5 @@ def purchase_create(request):
             elif not purchase_legitimate:
                 messages.error(request, "Your purchase could not be added")
                 return redirect("purchase_create")
-        
-        else:
-            print("Invalid")
 
     return render(request, 'purchase_create.html', context)
